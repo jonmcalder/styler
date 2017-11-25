83ce1e2fdae484e6179ddc4268a3af7f4441204d
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-11-25 20:19:34 +0100

    add tests

--- tests/testthat/test-public_api.R
+++ tests/testthat/test-public_api.R
@@ -25,15 +25,7 @@ test_that("styler does not return error when there is no file to style", {
   expect_error(style_dir(testthat_file("public-api", "xyzemptydir"), strict = FALSE), NA)
 })
 
-
-
-##  ............................................................................
-##  highlighted region                                                      ####
-
-# styling active region cannot be tested automatically since
-# rstudioapi::insertText() needs the context id.
-
-context("public API - Rmd")
+context("public API - Rmd in style_file()")
 
 test_that("styler can style Rmd file", {
   expect_false(
@@ -52,4 +44,14 @@ test_that("styler handles malformed Rmd file and invalid R code in chunk", {
   expect_warning(
     style_file(testthat_file("public-api", "xyzfile_rmd", "random4.Rmd"), strict = FALSE)
   )
+})
+
+context("public API - Rmd in style_dir()")
+
+test_that("styler can style Rmd fie via style_dir()", {
+  msg <- capture_messages(
+    style_dir(testthat_file("public-api", "xyz-r-and-rmd-dir"))
+  )
+  expect_true(any(grepl("random-script-in-sub-dir.R", msg, fixed = TRUE)))
+  expect_true(any(grepl("random-rmd-script.Rmd", msg, fixed = TRUE)))
 })
